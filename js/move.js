6c781acbc9b9fa6fce626a6c4fe7e8ff75b1e8f7
churuitao <38160761+churuitao@users.noreply.github.com>
2018-05-09 14:40:22 +0800

    Update move.js

--- js/move.js
+++ js/move.js
@@ -1,7 +1,7 @@
 //运动JS
 //包含 ：获取样式  弹性运动   缓冲运动
 
-//获取窗体的元素
+
 function onload_nav() {
     var oNav = document.getElementsByTagName('nav')[0];
     var aLi = oNav.getElementsByTagName('li');
@@ -18,7 +18,6 @@ function onload_nav() {
         })(i);
     }
 }
-
 
 //弹性运动
 var left=32;            //设置oDiv left 初始偏移默认值
@@ -50,24 +49,49 @@ function bufferMove(obj, attr, iTarget)
         var speed=(iTarget-temp)/6;
         speed=speed>0?Math.ceil(speed):Math.floor(speed);
 
-        if(temp==iTarget)
-        {
+        if(temp==iTarget){
             clearInterval(obj.timer);
-        }
-        else
-        {
-            if(attr=='opacity')
-            {
-                // obj.style.filter='alpha(opacity:'+(temp+speed)+')';
+        } else{
+            if(attr=='opacity'){
+                obj.style.filter='alpha(opacity:'+(temp+speed)+')';
                 obj.style.opacity=(temp+speed)/100;
-            }
-            else
-            {
+            }else{
                 obj.style[attr]=temp+speed+'px';
             }
         }
     }, 30);
 }
+
+//注册切换效果运动
+function loginMove(obj,attr,speed) {
+    clearInterval(obj.timer);
+    var trans = 0;
+    var loginMain = document.getElementById('login-main');
+    var oFrom = loginMain.getElementsByTagName('form');
+    obj.timer=setInterval(function (){
+        if(trans==90){
+            clearInterval(obj.timer);
+            if(getStyle(oFrom[0],'display')=='block'){
+                intoRegister();
+                clearLogin();
+            }
+            else{
+                backLogin();
+                clearegister();
+            }
+            obj.timer=setInterval(function (){
+                if(trans==0){
+                    clearInterval(obj.timer);
+                }else{
+                    setStyle(obj,'transform','rotate'+attr+'('+(--trans)+'deg)');
+                }
+            },speed);
+        }else{
+            setStyle(obj,'transform','rotate'+attr+'('+(++trans)+'deg)');
+        }
+    },speed);
+}
+
 
 //显示返回顶部按钮
 function showToTop(obj) {
@@ -81,9 +105,8 @@ function hiddenToTop(obj) {
 //返回页面顶部
 var to_Top=null;
 function toTop() {
-    clearInterval(to_Top);
     to_Top=setInterval(function () {
-        console.log(document.documentElement.scrollTop)
+        console.log(document.documentElement.scrollTop);
         if(document.documentElement.scrollTop>0){
             document.documentElement.scrollTop-=20;
         }else{
@@ -91,6 +114,40 @@ function toTop() {
         }
     },10);
 }
-
-
+//拖拽用户信息框
+function onload_userinfomove (){
+    var userinfo=document.getElementById('userinfo');
+    var disX=0;
+    var disY=0;
+    userinfo.onmousedown=function (ev){
+        var oEvent=ev||event;
+        disX=oEvent.clientX-userinfo.offsetLeft;
+        disY=oEvent.clientY-userinfo.offsetTop;
+        document.onmousemove=function (ev){
+            var oEvent=ev||event;
+            var left=oEvent.clientX-disX;
+            var top=oEvent.clientY-disY;
+            if(left<0){
+                left=0;
+            }else if(left>document.body.offsetWidth-userinfo.offsetWidth) {
+                left=document.body.offsetWidth-userinfo.offsetWidth;
+            }
+            if(top<0)
+            {
+                top=0;
+            }
+            else if(top>document.body.offsetHeight-userinfo.offsetHeight){
+                top=document.body.offsetHeight-userinfo.offsetHeight
+            }
+            userinfo.style.left=left+'px';
+            userinfo.style.top=top+'px';
+        };
+        //鼠标松开事件结束
+        document.onmouseup=function (){
+            document.onmousemove=null;
+            document.onmouseup=null;
+        };
+        return false;           //阻止firefox的BUG
+    };
+};
 
