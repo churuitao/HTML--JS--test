4e2eb0c1535b75dc34ac05e7586d250e892e5b33
churuitao <38160761+churuitao@users.noreply.github.com>
2018-05-09 14:47:13 +0800

    Create waterfall.js

---
+++ css/waterfall.js
@@ -0,0 +1,7 @@
+* { margin: 0; }
+header{margin-top: 30px;text-align: center;color: #505}
+.container{margin-top: 30px;}
+.container-bg{margin: auto; position: relative;}
+.box { float: left; padding: 10px; border: 1px solid #ccc; background: #f7f7f7; box-shadow: 0 0 8px #ccc; }
+.box:hover { box-shadow: 0 0 10px #999; }
+.box img { width: 240px; }
